4fa7c669e28259c20963e98f50339483052c0c06
Anand14879 <anuraganand2078@gmail.com>
2023-12-24 14:20:40 +0545

    Third Commit:Login and Registration working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Login.css"; // Make sure this CSS file is created with the styles for your login form
 import Header from "./Header";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  useEffect(() => {
-    if (localStorage.getItem("user-info")) {
-      navigate("/home", { replace: true });
-    }
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
+
+  async function logIn(e) {
+    e.preventDefault();
+    let item = { email, password };
+    let response = await fetch("http://127.0.0.1:8000/api/login", {
+      method: "POST",
+      body: JSON.stringify(item),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    let result = await response.json();
+    localStorage.setItem("user-info", JSON.stringify(result));
+    if (response.ok) {
+      navigate("/venues", { replace: true });
+    } else {
+      setError(result.message || "An error occurred during registration.");
+    }
+  }
 
   return (
     <>
@@ -18,12 +36,23 @@ function Login() {
         <div className="login-card">
           <div className="card-body">
             <h2 className="login-title">Login</h2>
-            <form>
+            {error && <div className="error-message">{error}</div>}
+            <form onSubmit={logIn}>
               <div className="form-group">
-                <input type="email" id="email" placeholder="Enter email" />
+                <input
+                  type="email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter email"
+                />
               </div>
               <div className="form-group">
-                <input type="password" id="password" placeholder="Password" />
+                <input
+                  type="password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                />
               </div>
               <div className="form-group">
                 <button type="submit">Login</button>
