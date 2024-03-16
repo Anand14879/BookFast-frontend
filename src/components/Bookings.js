31250ebc1bbb129ca484ddcfa2189ec987d9419e
Anand14879 <anuraganand2078@gmail.com>
2024-03-16 20:26:55 +0545

    Fixed bookings page

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import CategoryBookingsList from "./CategoryBookingList";
-//This is the bookings page where we can see all of the bookings made or which are pending
+// This is the bookings page where we can see all of the bookings made or which are pending
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
   const [error, setError] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem("user-info");
+    if (userInfo) {
+      const user = JSON.parse(userInfo);
+      setUserId(user.id); // Assuming that the ID is stored with the key 'id'
+    }
+  }, []);
 
   useEffect(() => {
     const fetchBookings = async () => {
       try {
-        const response = await fetch("http://127.0.0.1:8000/api/bookings");
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/bookings/${userId}`
+        );
         if (!response.ok) {
           throw new Error(`HTTP error ${response.status}`);
         }
@@ -20,8 +31,10 @@ const Bookings = () => {
       }
     };
 
-    fetchBookings();
-  }, []);
+    if (userId) {
+      fetchBookings();
+    }
+  }, [userId]); // userId is included as a dependency here
 
   return (
     <>
